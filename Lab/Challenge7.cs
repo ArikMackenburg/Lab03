da7349bd627e2290d79fbe1e1be5f1fec825381c
ArikMackenburg <Arikmackenburg@gmail.com>
2020-08-26 18:29:32 -0500

    finished challenges through 8

---
+++ Lab/Challenge7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection.Metadata;
+
+namespace Lab
+{
+    public class Challenge7
+    {
+        public static void WriteTextFile()
+        {
+            string text = File.ReadAllText(@"C:\Users\ArikM\OneDrive\Desktop\Delta\Lab03\Lab\words.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
