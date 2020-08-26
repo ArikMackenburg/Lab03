eaa1af197c8132c4249c4cb11b30319be1995e44
ArikMackenburg <Arikmackenburg@gmail.com>
2020-08-26 17:32:05 -0500

    challenge 4 passing tests

---
+++ Lab.Test/Challenge4.Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lab;
+using Xunit;
+
+namespace Lab.Test
+{
+    public class Challenge4Test
+    {
+        [Fact]
+        public static void CheckNumbers()
+        {
+            int[] arr = new int[] { 1, 2, 2, 2, 3, 3, 3, 4, 4, 5 };
+            int[] result = Challenge4.IdentifyNumbers(arr);
+            Assert.Equal(new int[]{ 1,2,3,4,5},result);
+        }
+
+        [Fact]
+        public static void CheckScore()
+        {
+            int[] arr = new int[] { 1, 2, 2, 2, 3, 3, 3, 4, 4, 5 };
+            int[] result = Challenge4.Score(arr);
+            Assert.Equal(new int[] { 1, 3, 3, 2, 1 }, result);
+        }
+        [Fact]
+        public static void CheckWinner()
+        {
+            int[] arr = new int[] { 1, 2, 2, 2, 3, 3, 3, 4, 4, 5 };
+            int[] score = new int[] { 1, 3, 3, 2, 1 };
+            int result = Challenge4.Winner(arr,score);
+            Assert.Equal(2, result);
+        }
+    }
+}
