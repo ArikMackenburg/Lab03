d169dcc1a8bdee40869c7628b292004296aaed29
ArikMackenburg <Arikmackenburg@gmail.com>
2020-08-27 10:18:45 -0500

    fixed largest based on input

--- Lab/Challenge5.cs
+++ Lab/Challenge5.cs
@@ -8,7 +8,7 @@ namespace Lab
     {
         public static int GetLargest(int[] arr)
         {
-            int largest = 0;
+            int largest = arr[0];
             for ( int i = 0; i < arr.Length; i++)
             {
                 if( largest < arr[i])
